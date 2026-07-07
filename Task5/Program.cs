f2f0e0fecbff3d381aa35c533b634528f26dcc3b
Reem Alshabibi <bintalshabibi11@gmail.com>
2026-07-07 23:45:06 +0400

    Complete Task5 - 1 - Fixed Grades Array

---
+++ Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+// Task 1 - Fixed Grades Array
+
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }    }
+}
